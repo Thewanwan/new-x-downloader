bb8fc27ff5e93da7910a1c593884e6c5856f988b
Thewanwan <noreply@users.noreply.github.com>
2026-06-21 23:12:10 +0800

    Add Android 15 predictive back support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
 
     // Activity & Lifecycle
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 
--- app/src/main/java/com/twitter/downloader/MainActivity.kt
+++ app/src/main/java/com/twitter/downloader/MainActivity.kt
@@ -2,6 +2,7 @@ package com.twitter.downloader
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
